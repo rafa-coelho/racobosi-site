4f34a5c86a89e97102b765bdbf9517eda6e1375f
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2021-12-31 14:34:35 -0300

    added: seção clientes & Responsividade

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -91,7 +91,7 @@ function SetSkillCardsText() {
         console.log(i <= 3 ? "top" : "bottom");
 
         var divCol3 = document.createElement("div");
-        divCol3.setAttribute("class", "col-md-3");
+        divCol3.setAttribute("class", "col-4 col-md-3");
 
         var divCard = document.createElement("div");
         divCard.setAttribute("class", "card");
@@ -134,12 +134,111 @@ function SetSkillCardsText() {
     // });
 }
 
+function SetProjectsCards() {
+    const projetos = [{
+            title: "Fichas de RPG",
+            description: "Sistema de gerenciamento de Fichas de RPG focado no Dungeons and Dragons",
+            url: "https://github.com/rafa-coelho/SistemaFichas",
+            stack: "React JS"
+        },
+        {
+            title: "Site de Casamento",
+            description: "Sistema que eu usei para o meu site de casamento com lista de compras, pagamento online e confirmação de presença!",
+            url: "https://github.com/rafa-coelho/marriage-list-api",
+            stack: "React JS"
+        },
+        {
+            title: "Feature Toggle",
+            description: "Aplicação usada para gerenciar funcionalidades em ambientes diferentes para poder ligar ou desligá-las quando quiser.",
+            url: "https://github.com/rafa-coelho/feature-toggle-api",
+            stack: "NodeJS (Typescript)"
+        },
+        {
+            title: "Client da API do Pagseguro",
+            description: "Pacote NPM para consumir a API de pagamento transparente do PagSeguro",
+            url: "https://www.npmjs.com/package/node-pagseguro",
+            stack: "NodeJS"
+        },
+    ];
+
+    let html = '';
+    for (const projeto of projetos) {
+        html += `<div class="col-md-3 mb-3 d-flex align-items-stretch">`;
+        html += `    <div class="card p-2">`;
+        html += `        <div class="card-body">`;
+        html += `            <h5 class="card-title"> <a target="blank" href="${projeto.url}">${projeto.title}</a></h5>`;
+        html += `            <h6 class="card-subtitle mb-2 text-muted">${projeto.stack}</h6>`;
+        html += `            <p class="card-text pt-3">`;
+        html += `                ${projeto.description}`;
+        html += `            </p>`;
+        html += `        </div>`;
+        html += `        <div class="card-footer">`;
+        html += `            <a target="blank" href="${projeto.url}">Ver mais</a>`;
+        html += `        </div>`;
+        html += `    </div>`;
+        html += `</div>`;
+    }
+
+    document.getElementById("projetosRealizados").innerHTML = html;
+}
+
+function SetClientsCards() {
+
+    const clientes = [{
+            title: "Prefácio",
+            subtitle: "Portal de Controle de Vendas",
+            stack: "ReactJS e NodeJS",
+            company: "A Prefácio é uma empresa de confecção e venda de paramentos religiosos.",
+            project: "Foi criado um sistema de gerenciamento de Pedidos e Clientes com gerenciamento de produção, estoque e com controle de acessos por níveis.",
+            image: "prefacio.png"
+        },
+        {
+            title: "Club7",
+            subtitle: "Plataforma, Aplicativo e Aplicação BackOffice",
+            stack: "NextJS, NodeJS e Flutter",
+            company: "Club7 é uma plataforma onde estabelecimentos e motoristas de aplicativo se encontram.",
+            project: "Foi criado o sistema de mapeamento, o Portal para estabelecimentos e o Aplicativo para os motoristas, contando com um sistema de cobrança recorrente de assinatura do serviço e uma aplicação de gerenciamento de clientes.",
+            image: "club7.png"
+        },
+        {
+            title: "LeadThis",
+            subtitle: "Plataforma de Geração e Venda de Leads",
+            stack: "NodeJS e ReactJS",
+            company: "O LeadThis era uma plataforma onde os corretores de planos de saúde podiam encontrar clientes em potencial.",
+            project: "Foi criado o sistema de captura de Leads e a Plataforma onde o Corretor podia comprar a Lead que mais lhe interessasse.",
+            image: "leadthis.png"
+        },
+    ];
+
+    let html = '';
+    for (const cliente of clientes) {
+        html += `<div class="col-xs-12 col-sm-12 col-md-4 d-flex align-items-stretch mb-3">`;
+        html += `    <div class="card">`;
+        html += `        <div class="card-body text-center">`;
+        html += `            <img src="/public/assets/images/${cliente.image}" class="rounded mx-auto d-block" alt="${cliente.title}">`;
+        html += `            <h5 class="card-title">${cliente.title}</h5>`;
+        html += `            <h6 class="card-subtitle text-muted pt-2">${cliente.subtitle}</h6>`;
+        html += `            <h6 class="card-subtitle text-muted pt-2">${cliente.stack}</h6>`;
+        html += `            <p class="card-text pt-3 ">`;
+        html += `                ${cliente.company}`;
+        html += `            </p>`;
+        html += `            <b>O que foi feito</b>`;
+        html += `            <p class="card-text pt-2">`;
+        html += `                ${cliente.project}`;
+        html += `            </p>`;
+        html += `        </div>`;
+        html += `    </div>`;
+        html += `</div>`;
+    }
+
+    document.getElementById("clientesAtendidos").innerHTML = html;
+}
+
 document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
     if (textArray.length) setTimeout(type, newTextDelay + 250);
 
     SetSkillCardsText();
     SetToolTips();
-
-
-
+    SetProjectsCards();
+    SetClientsCards();
 });
