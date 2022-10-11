3c13a4b13e244ab0e1d4c6fee645e39fd0a96af5
Rafael Coelho <rafaelcoelho8@yahoo.com.br>
2022-10-11 17:42:46 -0300

    added: verification from ga cookie

--- controllers/public/MainController.php
+++ controllers/public/MainController.php
@@ -13,12 +13,8 @@ class MainController extends Controller
         $this->views->title = "Tela um";
         $this->views->display("home.phtml");
 
-        if (!isset($_COOKIE["_ra"])) {
-            setcookie("_ra", REQUEST_IP . "_", time() + YEAR_SECONDS, "/");
-        }
-
         $reference = isset($_GET["ref"]) ? $_GET["ref"] : "";
-        $firstAccess = !isset($_COOKIE["_ra"]) ? 1 : 0;
+        $firstAccess = !isset($_COOKIE["_ga"]) ? 1 : 0;
 
         PageAccess::SaveAccess($_SERVER["REDIRECT_URL"], REQUEST_IP, $reference, $firstAccess);
     }
